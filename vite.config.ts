8a6983f3162853f3b10feb74dba789ab03818eb8
Ryan Balla <ryan@ryanballa.com>
2024-07-12 13:39:27 -0500

    Localhost config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		host: '127.0.0.1'
 	}
 	// css: {
 	// 	preprocessorOptions: {
