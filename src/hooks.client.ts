13ac12f99d55bd449f6dd04e89c1fc0bddab3fab
Ryan Balla <ryan@ryanballa.com>
2025-08-23 10:11:33 -0500

    Revert "Adding Sentry"
    This reverts commit 993045b2f69f7a7dc3279021aa6b41f1bd59c43f.

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,13 +1,5 @@
-import * as Sentry from '@sentry/sveltekit';
+import type { HandleClientError } from '@sveltejs/kit';
 
-Sentry.init({
-	dsn: 'https://dbe0f009be9647e1878b4452adaa2694@o820586.ingest.us.sentry.io/5809225',
-	// Adds request headers and IP for users, for more info visit:
-	// https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
-	sendDefaultPii: true,
-	integrations: []
-});
-const myErrorHandler = ({ error, event }) => {
-	console.error('An error occurred on the client side:', error, event);
+export const handleError: HandleClientError = async ({ error, event }) => {
+	console.error(error, event);
 };
-export const handleError = Sentry.handleErrorWithSentry(myErrorHandler);
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { sentrySvelteKit } from '@sentry/sveltekit';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'locomotivehouse',
-				project: 'locomotivehouse',
-				// store your auth token in an environment variable
-				authToken: process.env.SENTRY_AUTH_TOKEN
-			}
-		}),
-		sveltekit()
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
